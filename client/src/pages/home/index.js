479803b18bdee3162b6fb46b8439a202f8b48472
LauriHavi <lauri.havi@gmail.com>
2022-12-21 14:13:19 +0200

    first commit

---
+++ client/src/pages/home/index.js
@@ -0,0 +1,44 @@
+import styles from './styles.module.css';
+
+import { useNavigate } from 'react-router-dom';
+
+const Home = ({ username, setUsername, room, setRoom, socket }) => {
+
+    const navigate = useNavigate();
+
+    const joinRoom = () => {
+        if (room !== '' && username !== '') {
+          socket.emit('join_room', { username, room });
+        }
+
+        navigate('/chat', { replace: true });
+    };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
+        <h1>{`Chatroom Demo`}</h1>
+        <input 
+            className={styles.input} 
+            placeholder='Username...' 
+            onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <select 
+            className={styles.input}
+            onChange={(e) => setRoom(e.target.value)}
+        >
+          <option>-- Select Room --</option>
+          <option value='room1'>Room 1</option>
+          <option value='room2'>Room 2</option>
+          <option value='room3'>Room 3</option>
+          <option value='room4'>Room 3</option>
+        </select>
+
+        <button className='btn btn-secondary' style={{ width: '100%' }} onClick={joinRoom}>Join Room</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
